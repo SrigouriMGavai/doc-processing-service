157aacfb1f9fca8f01077e1693199ae810c7db7e
SrigouriMGavai <srigourimgavai10@gmail.com>
2025-12-15 15:36:41 +0530

    Initialize Go backend project structure

---
+++ cmd/api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Document Processing API starting...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/srigourimgavai-group/doc-processing-service
+
+go 1.24.5
